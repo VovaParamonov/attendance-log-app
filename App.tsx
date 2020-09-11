6a170d30bc34a0a0c1d182a7d861564cf0e5ea54
Paramonov Vladimir <44196011+VovaParamonov@users.noreply.github.com>
2020-09-11 22:57:54 +0600

    init commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello, my World!</Text>
       <StatusBar style="auto" />
     </View>
   );
